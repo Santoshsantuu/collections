0f90dd243e3caa958ede682fe2c034b148e83bd0
santosh <santoshjoy41@gmail.com>
2024-05-07 16:23:09 +0530

    collections

---
+++ src/Collections/swigyy.java
@@ -0,0 +1,16 @@
+package Collections;
+
+public class swigyy {
+    public String name;
+    public String rating;
+
+
+    public static void main(String[] args) {
+
+        swigyy swigyy = new swigyy();
+        swigyy.name="Mehfil";
+        swigyy.rating = "4.5";
+
+        System.out.println(swigyy);
+    }
+}
